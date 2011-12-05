bdbbdaeb25a406b48b7e66c9cb1c1467c87a32bb
usrr <usrr@online.de>
2011-12-05 16:21:20 +0100

    typo

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println( "Wohin wird sich dieses Projekt wohl entwickeln)");
+        System.out.println( "Wohin wird sich dieses Projekt wohl entwickeln?");
 
     }
 }
